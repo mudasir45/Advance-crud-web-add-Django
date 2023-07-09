aa24ff14301b0e3a9690e1d8922e4c5e1b35f1bf
Mudassar Amin <mudasiramin456@gmail.com>
2023-07-10 00:03:41 +0500

    commit

--- crud_app/main/urls.py
+++ crud_app/main/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('insert/', views.insert, name='insert'),
+    path('deleteStd/<int:id>', views.deleteStd, name='deleteStd'),
+    path('updateStd/<int:id>', views.updateStd, name='updateStd'),
+    
 ]
--- crud_app/main/views.py
+++ crud_app/main/views.py
@@ -29,3 +29,39 @@ def insert(request):
         messages.success(request, 'Sutdent insert successfully')
         return redirect('/')
     return render(request, 'form.html')
+
+def deleteStd(request, id):
+    try:   
+        std_obj = student.objects.get(id = id)
+        std_obj.delete()
+    except Exception as e:
+        print(e)
+
+    messages.info(request, 'Student Deleted Sucessfully!')
+    return redirect('/')
+
+
+def updateStd(request, id):
+    obj = student.objects.filter(id=id).first()
+    Students = student.objects.all()
+    if request.method == 'POST':
+        std_name = request.POST.get('std_name')
+        dept = request.POST.get('dept')
+        section = request.POST.get('section')
+        subject = request.POST.get('subject')
+        
+        std_obj = student.objects.get(id = id)
+        std_obj.name = std_name
+        std_obj.department = dept
+        std_obj.section = section
+        std_obj.subject = subject
+        std_obj.save()
+        messages.info(request, 'Student updated Sucessfully!')
+        return redirect('/')
+    label = True
+    context = {
+        'obj':obj,
+        'label':label,
+        'Students':Students,
+    }
+    return render(request, 'index.html', context)
