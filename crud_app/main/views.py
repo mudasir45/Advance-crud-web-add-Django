4366d3fd3b294641d1b75aaeee572c8088b4e7b4
Mudassar Amin <mudasiramin456@gmail.com>
2023-07-12 10:33:13 +0500

    commit

--- crud_app/main/views.py
+++ crud_app/main/views.py
@@ -30,6 +30,7 @@ def insert(request):
         return redirect('/')
     return render(request, 'form.html')
 
+
 def deleteStd(request, id):
     try:   
         std_obj = student.objects.get(id = id)
